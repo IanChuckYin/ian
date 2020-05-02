bebb21b46ebc2bacc88e2c1f3d1d56eab5dd41ce
Ian Chuck-Yin <iancy@github.com>
2020-05-02 18:38:57 -0400

    Website Remaster

---
+++ precache-manifest.74daa48103e71721412a07434054236c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "594a260f64497f067e5f23bdf491000d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "451bf69834bab6a37dd8",
+    "url": "/static/css/main.f8d7594b.chunk.css"
+  },
+  {
+    "revision": "da9f45ffc71e9449db28",
+    "url": "/static/js/2.a485f4f4.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.a485f4f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "451bf69834bab6a37dd8",
+    "url": "/static/js/main.1b28eb47.chunk.js"
+  },
+  {
+    "revision": "f3eb65ec2185793bbea6",
+    "url": "/static/js/runtime-main.43fa1b8f.js"
+  },
+  {
+    "revision": "a66a869f15b6343ae9c1bd6aa0a44c76",
+    "url": "/static/media/c_logo.a66a869f.png"
+  },
+  {
+    "revision": "78ba9e222b6ce5cad4dc6e5bf4b304ca",
+    "url": "/static/media/clojure_logo.78ba9e22.png"
+  },
+  {
+    "revision": "7531ac85afcc5c135a07fdb51fc4ce8b",
+    "url": "/static/media/java_logo.7531ac85.png"
+  },
+  {
+    "revision": "1cff25ecb30cb7b4a1e54a0ed53fb22d",
+    "url": "/static/media/js_logo.1cff25ec.png"
+  },
+  {
+    "revision": "29d302681e56304fea530d72910da454",
+    "url": "/static/media/la-icon.29d30268.png"
+  },
+  {
+    "revision": "a7e7f88aa8972942ee1c313519612668",
+    "url": "/static/media/la-image.a7e7f88a.png"
+  },
+  {
+    "revision": "65232491e4595c0c0bea90458771ee14",
+    "url": "/static/media/python_logo.65232491.png"
+  },
+  {
+    "revision": "8d52bff8c49947e02d21204d00cdafd4",
+    "url": "/static/media/slider-bg.8d52bff8.jpg"
+  },
+  {
+    "revision": "50156c71fb2cf47d5cfe134231975922",
+    "url": "/static/media/sql_logo.50156c71.png"
+  },
+  {
+    "revision": "85e2960ff9b5ceccbe3b9816b9a08394",
+    "url": "/static/media/uhn-image.85e2960f.png"
+  },
+  {
+    "revision": "05a1b606f990418c28d4586dd621db54",
+    "url": "/static/media/uhn-logo.05a1b606.png"
+  },
+  {
+    "revision": "12074315ad5ce98a0bfa35cd7aa15597",
+    "url": "/static/media/uncharted-image.12074315.png"
+  },
+  {
+    "revision": "bbbf8d57f06f4041ea2016ba757a8e5a",
+    "url": "/static/media/uncharted-logo.bbbf8d57.png"
+  },
+  {
+    "revision": "984874d19d1cdef8dcee5fcea55c082d",
+    "url": "/static/media/uwai-image.984874d1.png"
+  },
+  {
+    "revision": "d018832be9add6a10a986b67b9bcd6c2",
+    "url": "/static/media/uwai-logo.d018832b.png"
+  },
+  {
+    "revision": "e3618bb403d53e2ac927ee6246754896",
+    "url": "/static/media/waterloo-image.e3618bb4.png"
+  },
+  {
+    "revision": "cb9abac6570d9e747c4a7be721e68178",
+    "url": "/static/media/waterloo-logo.cb9abac6.png"
+  },
+  {
+    "revision": "a28b4fa7bd2fedcdb8997b2d6f3c159a",
+    "url": "/static/media/youtube_logo.a28b4fa7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,39 @@
 /**
- * @license
- * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
- * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
- * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
- * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
- * Code distributed by Google as part of the polymer project is also
- * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
  */
 
-/* eslint no-console: ["error", { allow: ["info"] }] */
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-console.info(
-  'Service worker disabled for development, will be generated at build time.'
+importScripts(
+  "/precache-manifest.74daa48103e71721412a07434054236c.js"
 );
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
