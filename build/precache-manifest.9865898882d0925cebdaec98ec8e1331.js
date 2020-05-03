8da889991f0d2639911eebaa7da66747807d660f
Ian Chuck-Yin <iancy@github.com>
2020-05-02 22:48:48 -0400

    build

---
+++ build/precache-manifest.9865898882d0925cebdaec98ec8e1331.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e8399772fc094866c813e643b33ec84",
+    "url": "/ian-portfolio/index.html"
+  },
+  {
+    "revision": "038b44623d3430263af8",
+    "url": "/ian-portfolio/static/css/main.01c493d2.chunk.css"
+  },
+  {
+    "revision": "8ad8bb9f4887e62cd295",
+    "url": "/ian-portfolio/static/js/2.d3f34e12.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ian-portfolio/static/js/2.d3f34e12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "038b44623d3430263af8",
+    "url": "/ian-portfolio/static/js/main.b03b9418.chunk.js"
+  },
+  {
+    "revision": "bc9f3c0e025aed7bea65",
+    "url": "/ian-portfolio/static/js/runtime-main.a987c355.js"
+  },
+  {
+    "revision": "a66a869f15b6343ae9c1bd6aa0a44c76",
+    "url": "/ian-portfolio/static/media/c_logo.a66a869f.png"
+  },
+  {
+    "revision": "78ba9e222b6ce5cad4dc6e5bf4b304ca",
+    "url": "/ian-portfolio/static/media/clojure_logo.78ba9e22.png"
+  },
+  {
+    "revision": "7531ac85afcc5c135a07fdb51fc4ce8b",
+    "url": "/ian-portfolio/static/media/java_logo.7531ac85.png"
+  },
+  {
+    "revision": "1cff25ecb30cb7b4a1e54a0ed53fb22d",
+    "url": "/ian-portfolio/static/media/js_logo.1cff25ec.png"
+  },
+  {
+    "revision": "29d302681e56304fea530d72910da454",
+    "url": "/ian-portfolio/static/media/la-icon.29d30268.png"
+  },
+  {
+    "revision": "a7e7f88aa8972942ee1c313519612668",
+    "url": "/ian-portfolio/static/media/la-image.a7e7f88a.png"
+  },
+  {
+    "revision": "65232491e4595c0c0bea90458771ee14",
+    "url": "/ian-portfolio/static/media/python_logo.65232491.png"
+  },
+  {
+    "revision": "8d52bff8c49947e02d21204d00cdafd4",
+    "url": "/ian-portfolio/static/media/slider-bg.8d52bff8.jpg"
+  },
+  {
+    "revision": "50156c71fb2cf47d5cfe134231975922",
+    "url": "/ian-portfolio/static/media/sql_logo.50156c71.png"
+  },
+  {
+    "revision": "85e2960ff9b5ceccbe3b9816b9a08394",
+    "url": "/ian-portfolio/static/media/uhn-image.85e2960f.png"
+  },
+  {
+    "revision": "05a1b606f990418c28d4586dd621db54",
+    "url": "/ian-portfolio/static/media/uhn-logo.05a1b606.png"
+  },
+  {
+    "revision": "12074315ad5ce98a0bfa35cd7aa15597",
+    "url": "/ian-portfolio/static/media/uncharted-image.12074315.png"
+  },
+  {
+    "revision": "bbbf8d57f06f4041ea2016ba757a8e5a",
+    "url": "/ian-portfolio/static/media/uncharted-logo.bbbf8d57.png"
+  },
+  {
+    "revision": "984874d19d1cdef8dcee5fcea55c082d",
+    "url": "/ian-portfolio/static/media/uwai-image.984874d1.png"
+  },
+  {
+    "revision": "d018832be9add6a10a986b67b9bcd6c2",
+    "url": "/ian-portfolio/static/media/uwai-logo.d018832b.png"
+  },
+  {
+    "revision": "e3618bb403d53e2ac927ee6246754896",
+    "url": "/ian-portfolio/static/media/waterloo-image.e3618bb4.png"
+  },
+  {
+    "revision": "cb9abac6570d9e747c4a7be721e68178",
+    "url": "/ian-portfolio/static/media/waterloo-logo.cb9abac6.png"
+  },
+  {
+    "revision": "a28b4fa7bd2fedcdb8997b2d6f3c159a",
+    "url": "/ian-portfolio/static/media/youtube_logo.a28b4fa7.png"
+  }
+]);
