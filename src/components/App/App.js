306239a59b52fd09f775654eaaeed793e921e725
Ian Chuck-Yin <iancy@github.com>
2020-05-02 23:38:47 -0400

    test

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -28,6 +28,7 @@ class App extends Component {
 
   render() {
     const { routes } = this.state;
+    console.log(path);
     const renderedRoutes = (
       <Switch>
         {routes.map((route, index) => <Route key={index} path={path + route.path} component={route.component} />)}
